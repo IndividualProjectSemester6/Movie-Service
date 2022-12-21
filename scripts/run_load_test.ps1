de9f30511be3112ded8998c097294b752c49c5a2
Calin Peters <calinpeters98@hotmail.nl>
2022-12-21 13:08:17 +0100

    Attempt to automate grafana + influx

---
+++ scripts/run_load_test.ps1
@@ -0,0 +1,5 @@
+docker-compose -f docker-compose.yml up -d 
+Write-Output "--------------------------------------------------------------------------------------"
+Write-Output "Load testing with Grafana dashboard http://localhost:3000/"
+Write-Output "--------------------------------------------------------------------------------------"
+k6 run /tests/load_test.js --out json=test.json --out influxdb=http://localhost:8089/moviesservice
