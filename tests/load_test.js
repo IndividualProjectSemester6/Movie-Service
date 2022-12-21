5ff3009108eab8356f3d33d6a833b715c15653d6
Calin Peters <calinpeters98@hotmail.nl>
2022-12-21 09:54:27 +0100

    Migrated to self-hosted runner

--- tests/load_test.js
+++ tests/load_test.js
@@ -4,7 +4,7 @@ import { sleep } from 'k6';
 // Configuration options for the load test.
 export const options = {
     duration: '1m',
-    vus: 50,
+    vus: 150,
     thresholds: {
         http_req_failed: ['rate<0.01'],
         http_req_duration: ['p(95)<500']
@@ -13,7 +13,7 @@ export const options = {
 
 // Script for load test:
 export default function() {
-    //const res = http.get('http://10.136.0.115:80/movies-service/api/Movies')
-    const res = http.get('http://20.238.149.147/movies-service/api/Movies');
+    const res = http.get('http://10.136.0.115:80/movies-service/api/Movies')
+    //const res = http.get('http://20.238.149.147/movies-service/api/Movies');
     sleep(1);
 }
