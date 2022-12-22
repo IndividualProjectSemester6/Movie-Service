36a851eeacad5b411ba4117d91a89906d78807a1
Calin Peters <calinpeters98@hotmail.nl>
2022-12-22 09:53:33 +0100

    Fix error in threshold

--- tests/load_test.js
+++ tests/load_test.js
@@ -6,7 +6,7 @@ export const options = {
     duration: '1m',
     vus: 150,
     thresholds: {
-        failed_requests: ['rate<0.01'],
+        http_req_failed: ['rate<0.01'],
         http_req_duration: ['p(95)<200','p(99)<300'],
         checks: ['rate>0.95']
     },
