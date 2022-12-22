68ac5402e7638d89fe08e76fe05227f01f38fefc
Calin Peters <calinpeters98@hotmail.nl>
2022-12-22 12:31:17 +0100

    Removed check from thresholds

--- tests/load_test.js
+++ tests/load_test.js
@@ -8,7 +8,6 @@ export const options = {
     thresholds: {
         http_req_failed: ['rate<0.01'],
         http_req_duration: ['p(95)<200','p(99)<300'],
-        checks: ['rate>0.95']
     },
 }
 
